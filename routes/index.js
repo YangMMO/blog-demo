8c0fe302bd178b0a9b270a7e555884d577159a71
YangMMO <11436052@qq.com>
2018-09-23 12:41:57 +0800

    push

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', {
+    id: 10,
+    title: '2123132132',
+    content: 456,
+    date: 123
+  })
+  // res.sendfile('../views/home.html')
 });
 
 module.exports = router;
