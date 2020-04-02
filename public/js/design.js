9c04f4e69376ffb9b2609b650626ddc26f47dd8f
YangMMO <11436052@qq.com>
2020-04-02 14:13:54 +0800

    push

--- public/js/design.js
+++ public/js/design.js
@@ -1,28 +1,115 @@
 
-//计算公式
-//16：9  21：9  4：3  2：1
-var w = 16; //比例宽
-var h = 9; //比例高
+var cObj = [{   //常见比例
+        w: 16,
+        h: 9
+    }, {
+        w: 21,
+        h: 9
+    }, {
+        w: 4,
+        h: 3
+    }, {
+        w: 2,
+        h: 1
+    },{
+        w: 0,
+        h: 0
+    }]
 
-var scale = w / h;
-// var scale = 1.618;  //手输比例
+var scale = cObj[0].w / cObj[0].h; //计算比例值
 
-
-function calculate (x, y) {
+/**
+ * 换算器逻辑
+ * @param {Number} num 
+ * @param {Boolean} ops 
+ */
+function calculate (num, ops) {
     var result;
-    if (x === 'undefined' | y === 'undefined') {
+    if (num === 'undefined') {
         return;
     }
 
-    if (x > 0) {
-        thisB = x / scale;
-        return result;
+    if (num > 0 && ops === 0) {
+        result = num / scale;
+        return Math.round(result);
     }
 
-    if (y > 0) {
-        thisB = y * scale;
-        return result;
+    if (num > 0 && ops === 1) {
+        result = num * scale;
+        return Math.round(result);
     }
 }
 
-calculate(800,0);
+/* ------------------------------- */
+
+var calculator = document.getElementById('calculator');
+var cInp = calculator.querySelectorAll('input');
+
+var calculatorType = document.querySelectorAll('#calculator-type > li > a');
+
+var customizeInp = document.querySelectorAll('#calculator-type > li > a > input');
+
+for (var i = 0; i < calculatorType.length; i++) {
+    calculatorType[i].index = i;
+    calculatorType[i].addEventListener('click', function (e) {
+        for (var i = 0; i < calculatorType.length; i++) {
+            calculatorType[i].className = '';
+        }
+        this.className = 'c-active';
+        scale = cObj[this.index].w / cObj[this.index].h;
+    }, false)
+}
+
+/**
+ * 焦点事件
+ * @param {Node} inp 
+ */
+function onFocus (inp) {
+    inp.placeholder = ''
+}
+
+/**
+ * 失去焦点
+ * @param {Node} inp 
+ */
+function onBlur (inp) {
+    inp.placeholder = '输入';
+}
+
+
+/**
+ * 按键监听 & 换算
+ * @param {Object} event 
+ * @param {Node} inp 
+ * @param {Boolean} ops 
+ */
+function onKeyUp (event, inp, ops) {
+    var regex = /^[0]+/g;
+
+    if (event.keyCode === 13 && inp.value !== '') {
+        var reslut = calculate(inp.value, ops);
+        if (ops === 0) {
+            cInp[1].value = reslut;
+        } else {
+            cInp[0].value = reslut;
+        }
+        return
+    }
+
+    if (parseInt(event.key) >= 0) {
+        inp.value = inp.value.replace(regex,"")
+    } else {
+        inp.value = '';
+        return
+    }
+}
+
+
+
+function isCustomize () {
+    if (customizeInp[0].value !== '' && customizeInp[1].value !== '') {
+        cObj[4].w = parseInt(customizeInp[0].value);
+        cObj[4].h = parseInt(customizeInp[1].value);
+        scale = cObj[4].w / cObj[4].h;
+    }
+}
